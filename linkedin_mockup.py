8d0304f7d99d30eb93ad8b79c4be10fe5ea32a8a
bdewhirst <b.dewhirst@gmail.com>
2022-07-13 14:24:24 -0400

    add class for linkedin banner

---
+++ linkedin_mockup.py
@@ -0,0 +1,15 @@
+class LinkedInMock:
+    """
+    example python class for LinkedIn Banner
+    """
+    def __init__(self, some_var: float, is_pep8: bool) -> None:
+            self.some_var = 0.0
+            self.is_pep8 = True
+
+    def brag(self) -> None:
+        if self.is_pep8 is True:
+            print(f"This code complies with pep8, since is_pep8 equals {self.is_pep8}.")
+        elif self.some_var == 0.0:
+            self.some_var += 1
+        else:
+            raise ValueError("something went wrong")
