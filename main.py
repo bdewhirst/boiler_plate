264e72c360bfa58b6a8d40b59c9bca5977971051
bdewhirst <b.dewhirst@gmail.com>
2022-07-13 13:21:52 -0400

    implement data retrieval

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import model.model
+import pandas as pd
+import numpy as np
+# import sk-learn as skl...
+
+import utils.common_utils as u
+import model.model as model
 
 
-model.model.hello()
+def demo() -> None:
+    """
+    do something (say 'hello world', in this case)
+    """
+    model.hello()
+
+
+def main() -> None:
+    """
+    Main execution path of function
+    :return: nothing
+    """
+    try:
+        conn = u.sqlite_connect()
+        train = u.run_sqlite_query(conn=conn, table_name="train")
+        # test = u.get_table(conn=conn, table_name="test")
+    finally:
+        if conn:
+            conn.close()
+    print("done with database retrieval")
+
+    print(train.head())
 
 
 
+if __name__ == '__main__':
+    demo()
+    main()
--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,4 +1,8 @@
+import sqlite3
+import os
+
 import pandas as pd
+
 
 
 def csv_loader(source_file: str,) -> pd.DataFrame:
@@ -21,3 +25,41 @@ def sniff_frame(df: pd.DataFrame) -> None:
     print(df.columns)
     print(df.shape)
     print(df.describe())  # we'd need to tweak the width
+
+
+def sqlite_connect():
+    """
+    Create a connection to the sqlite database specified by the included constants
+    :return: connection to database
+    """
+
+    YOUR_PREFERRED_DIRECTORY = r"C:\Users\Brian\sqlite\db"
+    DB_NAME = "pythonsqlite.db"
+
+    db_file = os.path.join(YOUR_PREFERRED_DIRECTORY, DB_NAME)
+    try:
+        conn = sqlite3.connect(db_file)
+        ("connecting to sqlite-- remember to exit cleanly")  # e.g., see finally... if conn... commented lines below
+    except sqlite3.Error as e:
+        print(e)
+    # finally:
+    #     if conn:
+    #         conn.close()
+    # print("done")
+
+    return conn
+
+
+def run_sqlite_query(conn, table_name,) -> pd.DataFrame:
+    """
+    Retrieve specified table via specified connection
+    :param conn: database connection
+    :param table_name: name of table/view to retrieve
+    :return: pandas dataframe
+    """
+    print("note-- currently this function is rather inflexible")
+
+    query = f"select * from {table_name}"
+    data= pd.read_sql_query(sql=query, con=conn)
+
+    return data
