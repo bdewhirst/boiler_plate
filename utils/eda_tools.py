aa30089622ce90b40c7251b5244960f3f525f9a8
bdewhirst <b.dewhirst@gmail.com>
2022-09-12 16:34:20 -0400

    tweak alpha, fix legend, refactor messages

--- utils/eda_tools.py
+++ utils/eda_tools.py
@@ -4,41 +4,36 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 
-def sniff_frame(df: pd.DataFrame) -> None:
+def say_tp_message(desc: str, strdata: str) -> None:
     """
-    run some standard sniff tests on the indicated dataframe
-    :param df: pandas dataframe
-    :return: nothing; function outputs to terminal
+    Print a simple (two part) message consisting of two strings
+    :param desc: string describing what data will be output next:
+    :param strdata: meaningful data represented as a string
+    :return: returns nothing, but does print to STDOUT
     """
-    pd.set_option("display.max_colwidth", None)
-    pd.set_option("display.width", 2000)
-    pd.set_option("display.max_columns", 10)
-
-    print(df.columns)
-    print(df.shape)
-    print(df.describe())
+    message = "".join([desc, strdata])
+    print(message)
 
 
 def verbose_sniff(data: pd.DataFrame) -> None:
     """
     Provide descriptive information about a given dataframe
-    :param data: pandas dataframe to be examined
+    :param data: pandas dataframe to be examined-- often from a call of a native pandas DataFrame method
     :return: nothing-- currently prints to STDOUT
     """
     pd.set_option("display.max_colwidth", None)
     pd.set_option("display.width", 2000)
     pd.set_option("display.max_columns", 10)
 
-    message = "".join(["data shape is ", str(data.shape)])
-    print(message)
-    message = "".join(["missing data by column: \r\n", str(data.isnull().sum())])
-    print(message)
-    message = "".join(["data head is: \r\n", str(data.head())])
-    print(message)
-    message = "".join(["pandas describe is: \r\n", str(data.describe())])
-    print(message)
-    message = "".join(["data types are: \r\n", str(data.dtypes)])
-    print(message)
+    say_tp_message(desc="data info is \r\n", strdata=str(data.info()))
+    say_tp_message(desc="data shape is ", strdata=str(data.shape))
+    say_tp_message(
+        desc="missing data by column: \r\n", strdata=str(data.isnull().sum())
+    )
+    say_tp_message(desc="data head is: \r\n", strdata=str(data.head()))
+    say_tp_message(desc="pandas describe is: \r\n", strdata=str(data.describe()))
+    say_tp_message(desc="data types are: \r\n", strdata=str(data.dtypes))
+    say_tp_message(desc="data types are: \r\n", strdata=str(data.dtypes))
 
 
 def do_correl_matrix(data: pd.DataFrame) -> None:
@@ -84,9 +79,10 @@ def do_small_multiples(data: pd.DataFrame, y_col: str) -> None:
         ncols = int(cells / 3) + 1
         plt.subplot(nrows, ncols, num)  # nrows, ncols, index
         # plt.plot(data[y_col], data[column], label=column)
-        plt.scatter(y=data[y_col], x=data[column], marker=",", label=column, alpha=0.8)
+        plt.scatter(y=data[y_col], x=data[column], marker=",", label=column, alpha=0.7)
         indep_min = data[column].min()
         indep_max = data[column].max()
+        plt.legend(loc=2)
         plt.xlim(indep_min, indep_max)
         plt.ylim(dep_min, dep_max)
     plt.show()
