aee4a18dd045853f39f0c779ac02e39fd3cf4fe5
bdewhirst <b.dewhirst@gmail.com>
2022-10-12 14:41:31 -0400

    move messaging function to utils

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -57,3 +57,14 @@ def run_sqlite_query(
     data = pd.read_sql_query(sql=query, con=conn)
 
     return data
+
+
+def say_tp_message(desc: str, strdata: str) -> None:
+    """
+    Print a simple (two part) message consisting of two strings
+    :param desc: string describing what data will be output next:
+    :param strdata: meaningful data represented as a string
+    :return: returns nothing, but does print to STDOUT
+    """
+    message = "".join([desc, strdata])
+    print(message)
