33a49892ecca8401d9e513cf1e207db16d0c0124
bdewhirst <b.dewhirst@gmail.com>
2022-10-12 14:42:23 -0400

    black and cleanup

--- utils/constants.py
+++ utils/constants.py
@@ -2,4 +2,8 @@ RAWCSV: str = "data/sundae-raw.csv"
 SEED: int = 20220913
 SAMPLE: int = 1000
 DEP_VAR_COL_NAME: str = "y_yes"
-INDEP_VAR_COL_NAMES: str = ["age", "cons.price.idx"]  # placeholder values which may not give great fit
+# placeholder values which may not give great fit
+INDEP_VAR_COL_NAMES: str = [
+    "age",
+    "cons.price.idx",
+]
