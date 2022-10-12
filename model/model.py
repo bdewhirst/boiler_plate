5fda598152302de552cfedda87554081e07c3f5e
bdewhirst <b.dewhirst@gmail.com>
2022-10-12 15:29:56 -0400

    refactor global naive model

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 import statsmodels.api as sm
 import pandas as pd
 import numpy as np
@@ -6,77 +8,98 @@ from sklearn import linear_model as skl_linear_model
 from sklearn.metrics import r2_score as skl_r2
 
 
-def split_xs_and_ys(
-    df: pd.DataFrame,
-    x_cols: list,
-    y_col: str,
-) -> tuple:
+class ModelPrep(ABC):
     """
-    :param df: dataframe to split into pre
-    :param x_cols: predictors of y
-    :param y_col: column to be solved for
-    :return: tuple of two numpy objects based on xs and y respectively
+    Abstract base class defining required functionality to prepare data for modeling
     """
-    xs = df[x_cols]
-    y = df[[y_col]]
-    return (xs, y)
+
+    @abstractmethod
+    def __init__(self, data, dep_var, indep_vars, test_size):
+        raise NotImplementedError(
+            "Prep must implement initialization of data, dependent and independent variables"
+        )
+
+    @abstractmethod
+    def _x_y_split(self):
+        raise NotImplementedError(
+            "Prep must implement a private method to split data into x and y"
+        )
+
+    @abstractmethod
+    def _filter_indep_cols(self):
+        raise NotImplementedError(
+            "Prep must implement a private method filter raw inputs to columns of interest"
+        )
+
+    @abstractmethod
+    def test_train_split(self):
+        raise NotImplementedError(
+            "Prep must implement a method to split the data into test and train"
+        )
 
 
-def do_test_train_split(
-    df: pd.DataFrame,
-    indep_vars: list,
-    dep_var: str,
-    test_size: float = 0.25,
-) -> tuple:
-    """
-    given a pandas dataframe and specified independent variable, perform a test train split and return tuple of all four
-    :param df: pandas dataframe
-    :param indep_vars: list of independent variable columns to train on
-    :param dep_var: dependent variable's column name
-    :param test_size: fractional value to determine size of holdout (test data)
-    :return: tuple of the matrices of independent variables to train on and corresponding train/test dependent variables
-    """
-    xs, y = split_xs_and_ys(
-        df=df,
-        x_cols=indep_vars,
-        y_col=dep_var,
-    )
-    x_train, x_test, y_train, y_test = skl_model_selection.train_test_split(
-        xs,
-        y,
-        test_size=test_size,
-    )
-    return x_train, x_test, y_train, y_test
+class SimpleModelPrep(ModelPrep):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        dep_var: str,
+        indep_vars: list,
+        test_size: float = 0.25,
+    ):
+        self.data = data
+        self.dep_var = dep_var
+        self.indep_vars = indep_vars
+        self.test_size = test_size
+
+    def _filter_indep_cols(self) -> pd.DataFrame:
+        return self.data[self.indep_vars]
+
+    def _x_y_split(self) -> tuple:
+        xs = self._filter_indep_cols()
+        y = self.data[[self.dep_var]]
+        return (xs, y)
+
+    def test_train_split(self) -> tuple:
+        xs, y = self._x_y_split()
+        # x_train, x_test, y_train, y_test
+        return skl_model_selection.train_test_split(
+            xs,
+            y,
+            test_size=self.test_size,
+        )
 
 
-def do_global_naive(y: pd.DataFrame) -> dict:
+class Model(ABC):
     """
-    Take the global average of all dependent training data
-    :param y: array of values of dependent variable
-    :return: dictionary of model type and fitted model as key/value pair
+    Abstract base class defining required functionality of a model
     """
-    # take a global average-- work on this further
-    y_mean = y.mean()
-    return {
-        "global_naive": y_mean,
-    }
+
+    # @abstractmethod
+    # def __init__(self, data):
+    #     raise NotImplementedError(
+    #         "Prep must implement initialization of data, dependent and independent variables"
+    #     )
+
+    @abstractmethod
+    def fit(self, indep_data, dep_data):
+        raise NotImplementedError(
+            "A model must implement a method to fit data to the model"
+        )
+
+    @abstractmethod
+    def apply(self, dep_data):
+        raise NotImplementedError(
+            "A model must implement a method to apply data to a fit model"
+        )
+
+    @abstractmethod
+    def score(self, true_data):
+        raise NotImplementedError(
+            "A model must implement a method to fit data to the model"
+        )
 
 
-def do_groupby_naive(
-    xs: pd.DataFrame,
-    y: pd.DataFrame,
-    group_cols: list,
-) -> dict:
-    """
-    Use pandas' groupby method to take the averages of each unique combination of columns grouped-by
-    :param xs:  (i.e. X) independent variables
-    :param y: 1-D dependent data to fit on by taking
-    :param group_cols: list of column names to aggregate on
-    :return: dictionary of a label and 'trained model' (averages by
-    """
-    raise ValueError("This model type isn't actually implemented yet")
-
-
+# statsmodels linear model
 def do_statsmodels_lm(
     xs: pd.DataFrame,
     y: pd.DataFrame,
@@ -92,47 +115,6 @@ def do_statsmodels_lm(
     est2 = est.fit()
     print(est2.summary())
     return {"sm_linear": est2}
-
-
-def do_lin_reg(
-    xs: pd.DataFrame,
-    y: pd.DataFrame,
-) -> dict:
-    """
-    Fit a linear regression on the specified columns using sklearn
-    :param xs: (i.e. X) pandas dataframe to fit linear regression to
-    :param y: pandas dataframe to fit linear regression to
-    :return: dictionary of model type and fitted model as key/value pair
-    """
-    regr = skl_linear_model.LinearRegression()
-    y = y.values.ravel()
-    fit_model = regr.fit(X=xs, y=y)
-    return {"sk_linear": fit_model}
-
-
-def score_global_naive(
-    y_test: pd.DataFrame,
-    fit_model: dict,
-) -> None:
-    """
-    shoehorn the static value into an array of same length as the test data and evaluate coefficient of determination
-    :param y_test:
-    :param fit_model:
-    :return: returns nothing, but prints to STDOUT
-    """
-    print("." * 10)
-    print("Accuracy metrics for global naive:")
-    y_test_label = y_test.columns
-    static_prediction = fit_model.iloc[0]
-    test_len = len(y_test)
-    same_len_array = np.arange(0, test_len, dtype=int)
-    y_test = y_test.values.ravel()
-    y_pred = pd.DataFrame(
-        data=static_prediction, index=same_len_array, columns=[y_test_label]
-    ).values.ravel()
-    r2 = skl_r2(y_true=y_test, y_pred=y_pred)  # strongly consider refactoring
-    print("global naive coefficient of determination (R^2) is: ", str(r2))
-    print("." * 10)
 
 
 def score_sm_linear_fit(
@@ -155,6 +137,23 @@ def score_sm_linear_fit(
     r2 = skl_r2(y_true=y_test, y_pred=y_pred)
     print("statsmodels coefficient of determination (R^2) is: ", str(r2))
     print("." * 10)
+
+
+# skl linear model
+def do_lin_reg(
+    xs: pd.DataFrame,
+    y: pd.DataFrame,
+) -> dict:
+    """
+    Fit a linear regression on the specified columns using sklearn
+    :param xs: (i.e. X) pandas dataframe to fit linear regression to
+    :param y: pandas dataframe to fit linear regression to
+    :return: dictionary of model type and fitted model as key/value pair
+    """
+    regr = skl_linear_model.LinearRegression()
+    y = y.values.ravel()
+    fit_model = regr.fit(X=xs, y=y)
+    return {"sk_linear": fit_model}
 
 
 def score_sk_linear_fit(
@@ -186,6 +185,7 @@ def score_sk_linear_fit(
     print("." * 10)
 
 
+# scikit-learn logistic regression
 def do_skl_logit(
     xs: pd.DataFrame,
     y: pd.DataFrame,
@@ -232,3 +232,57 @@ def score_sk_logistic(
         "\r\ncoefficients were: ",
         coefs,
     )
+
+
+class NaiveModel(Model):  # not fully implemented /tested
+    def _do_global_naive(self, indep_data: pd.DataFrame) -> dict:
+        """
+        Take the global average of all dependent training data
+        :param y: array of values of dependent variable
+        :return: dictionary of model type and fitted model as key/value pair
+        """
+        # take a global average-- work on this further
+        return {"global_naive": indep_data.mean()}
+
+    # def _do_groupby_naive(
+    #     xs: pd.DataFrame,
+    #     y: pd.DataFrame,
+    #     group_cols: list,
+    # ) -> dict:
+    #     """
+    #     Use pandas' groupby method to take the averages of each unique combination of columns grouped-by
+    #     :param xs:  (i.e. X) independent variables
+    #     :param y: 1-D dependent data to fit on by taking
+    #     :param group_cols: list of column names to aggregate on
+    #     :return: dictionary of a label and 'trained model' (averages by
+    #     """
+    #     raise ValueError("This model type isn't actually implemented yet")
+
+    def fit(self, indep_data, dep_data):
+        self.global_naive_fit = self._do_global_naive(indep_data=indep_data)
+
+    def apply(self, dep_data):
+        self.applied_result = self.global_naive_fit.get("global_naive")[
+            0
+        ]  # i.e., the only value in the series
+        # note: this model.apply _purposefully_ does nothing with dep_data-- a global naive model has a constant result
+
+    def score(self, true_data) -> None:
+        """
+        shoehorn the static value into an array of same length as the test data and evaluate coefficient of determination
+        :return: returns nothing, but prints to STDOUT
+        """
+        print("." * 10)
+        print("Accuracy metrics for global naive:")
+        y_test_label = true_data.columns
+        test_len = len(true_data)
+        same_len_array = np.arange(0, test_len, dtype=int)
+        y_test = true_data.values.ravel()
+        y_pred = pd.DataFrame(
+            data=self.applied_result,
+            index=same_len_array,
+            columns=[y_test_label],
+        ).values.ravel()
+        r2 = skl_r2(y_true=y_test, y_pred=y_pred)  # strongly consider refactoring
+        print("global naive coefficient of determination (R^2) is: ", str(r2))
+        print("." * 10)
--- utils/constants.py
+++ utils/constants.py
@@ -1,9 +1,18 @@
+from model import model
+
+
 RAWCSV: str = "data/sundae-raw.csv"
 SEED: int = 20220913
 SAMPLE: int = 1000
 DEP_VAR_COL_NAME: str = "y_yes"
-# placeholder values which may not give great fit
+# placeholder values which may not give great fit for current data
 INDEP_VAR_COL_NAMES: str = [
     "age",
     "cons.price.idx",
 ]
+SUPPORTED_MODEL_CLASSES: dict = {
+    "global_naive": model.NaiveModel,
+    #     "sm_linear": model.StatsModelsLinear,
+    #     "sk_linear": model.SciKitLearnLinear,
+    #     "logistic": model.SciKitLearnLogistic,
+}
